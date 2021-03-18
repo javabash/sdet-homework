bfafe26a03a4e5923196ae32967e1e23368b78fa
Philip GeLinas <phil.t.gelinas@gmail.com>
2021-03-17 19:35:15 -0700

    First Commit

---
+++ src/main/java/com/rei/sdethomework/Dictionary.java
@@ -0,0 +1,13 @@
+package com.rei.sdethomework;
+
+public class Dictionary {
+	
+	public static boolean isEnglishWord(String word) {
+		
+		
+		
+		return true;
+		
+	}
+
+}
---
+++ src/main/java/com/rei/sdethomework/Permutations.java
@@ -0,0 +1,28 @@
+package com.rei.sdethomework;
+
+import java.util.ArrayList;
+
+public class Permutations {
+	
+	public ArrayList getPerms(){
+		
+		ArrayList<String> permsArray = new ArrayList<String>();
+		
+		return permsArray;
+				
+	}
+	
+	
+	ArrayList generatePerms(ArrayList<String> arr) {
+				
+		
+		return arr;
+		
+		
+		
+	}
+	
+	
+
+
+}
